f959cf34dd46da16d0829385cc7ee33e7e84eeb9
Abdullah Muhammad Siam <abdullahms838@gmail.com>
2023-08-27 14:14:57 +0600

    new section added

--- src/pages/about/AboutMe.jsx
+++ src/pages/about/AboutMe.jsx
@@ -7,7 +7,7 @@ const AboutMe = () => {
             <p className='text-center mt-3 text-4xl mb-10'>- - | |  - -</p>
             <h1 className='text-center text-5xl font-bold'>ABOUT ME</h1>
 
-            <div className='grid grid-cols-2 my-auto gap-2  mt-6'>
+            <div className='grid grid-cols-2 my-auto gap-2 mt-6'>
                 <div className='w-full'>
                     <h1 className='text-3xl font-bold'>Personal Information </h1>
                     <div className='mt-2 flex gap-2 w-full'>
@@ -43,6 +43,7 @@ const AboutMe = () => {
                     </div>
                     <div className='border p-3 py-8 w-full rounded-md hover:bg-[#ffde29] hover:text-black pe-9 border-[#ffde29]'>
                         <hs1 className='text-2xl font-bold'>Tailwind</hs1>
+                        <h2 className='font-bold ms-2'>-- Expertise</h2>
                         <h2 className='font-bold ms-2'>-- Expertise</h2>
                     </div>
 
---
+++ src/pages/skills/Skills.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Skills = () => {
+    return (
+        <div>
+            <h2>hello</h2>
+        </div>
+    );
+};
+
+export default Skills;
